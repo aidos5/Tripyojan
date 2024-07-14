9e2bee90af946aeb807d57c505843305b51d3f7b
Harshith C S <107236868+harshith082003@users.noreply.github.com>
2024-07-14 08:04:15 +0530

    unit testing

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,14 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+import { getSum } from '../realLandingPage/smaple';
 
-test('goto html page', async ({page}) => {
-  await page.goto('http://127.0.0.1:5500/realLandingPage/index.html')
-  await expect(page).toHaveTitle(/Tripyojan.com/)
+// test('goto html page', async ({page}) => {
+//   await page.goto('http://127.0.0.1:5500/realLandingPage/index.html')
+//   await expect(page).toHaveTitle(/Tripyojan.com/)
+// })
+
+test('Some testing', () => {
+  const ans = getSum(2, 3);
+
+  expect(ans).toBe(5);
 })
